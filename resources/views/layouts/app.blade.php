12b02fb920020c78c868e26de89ee078607fe6d1
PratamaMF <fajarpratamavpc@gmail.com>
2025-07-14 00:52:51 +0800

    Force relative paths for CSS/JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta content="" name="keywords" />
     <meta content="" name="description" />
 
-    <link href="{{ asset('assets/img/Logo2.png') }}" rel="icon" />
+    <link href="/assets/img/Logo2.png" rel="icon" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -17,13 +17,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
 
-    <link href="{{ asset('assets/lib/animate/animate.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet" />
-
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+    <link href="/assets/lib/animate/animate.min.css" rel="stylesheet" />
+    <link href="/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="/assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -40,16 +38,16 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets/lib/wow/wow.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/waypoints/waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/counterup/counterup.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/parallax/parallax.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/lib/lightbox/js/lightbox.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    
+    <script src="/assets/lib/wow/wow.min.js"></script>
+    <script src="/assets/lib/easing/easing.min.js"></script>
+    <script src="/assets/lib/waypoints/waypoints.min.js"></script>
+    <script src="/assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="/assets/lib/counterup/counterup.min.js"></script>
+    <script src="/assets/lib/parallax/parallax.min.js"></script>
+    <script src="/assets/lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="/assets/lib/lightbox/js/lightbox.min.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 
 </html>
