c847e04856abd3153cdb58ec8ab53b696b173e5c
PratamaMF <fajarpratamavpc@gmail.com>
2025-08-04 17:04:44 +0800

    update

--- resources/views/event-card/2025.blade.php
+++ resources/views/event-card/2025.blade.php
@@ -4,11 +4,11 @@
 <div class="col-lg-4 col-md-6 portfolio-item wow fadeInUp" data-wow-delay="0.1s">
     <div class="team-item rounded">
         <div class="event-data"
-            data-poster-src="assets/img/event/pamali.png"
+            data-poster-src="../assets/img/event/pamali.png"
             data-title="Nonton Bareng PAMALI"
             data-description="PANGGILAN UNTUK VIEWERS B2F BANJARMASIN DAN SEKITARNYA! 📢\n\nSiapa yang udah nggak sabar untuk bertepuk tangan melihat akting super imba gelo dari bang aktor @aldeantg di film PAMALI?\n\nAyok, kita nonton bareng biar makin seru geyz! Khusus untuk kalian semua viewers B2F yang ada di Banjarmasin dan sekitarnya.\nLokasi Nobar:📍 XXI Duta Mall Banjarmasin\n\nWaktu Nobar: 🗓️ Akan ditentukan bersama di grup khusus!\n\nGimana cara daftarnya? Ez geyz! Langsung isi formulir pendaftaran di link yang ada di bio dan buka halaman event. Setelah mengisi data, nanti akan diundang untuk bergabung ke grup WhatsApp untuk diskusi jadwal nonton bareng."
             data-link="https://forms.gle/aavU1Vbnfjp78o4N9">
-            <img class="img-fluid" src="assets/img/event/pamali.png"/>
+            <img class="img-fluid" src="../assets/img/event/pamali.png"/>
         </div>
         <div class="team-text">
             <h4 class="mb-0">Nonton Bareng PAMALI</h4>
